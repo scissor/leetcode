6de5cbd8800fef004f6aea35b930e244a12bc6e9
scissor <scissorsmail@gmail.com>
2022-10-26 10:15:56 +0800

    [704] Binary Search

---
+++ Unknown-easy/704.binary-search.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=704 lang=python3
+#
+# [704] Binary Search
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0 
+        r = len(nums) - 1
+        
+        # index = math.floor(l + r)
+        index = (l + r) // 2
+        
+        while l <= r:
+            index = (l + r) // 2
+            if nums[index] < target:
+                l = index + 1
+            elif nums[index] > target:
+                r = index - 1
+            else:
+                return index
+            
+        return -1
+# @lc code=end
+
