4136e5c6427aae225825de93774a29fce9c1fdb1
scissor <scissorsmail@gmail.com>
2022-10-31 14:42:02 +0800

    [235] Lowest Common Ancestor of a Binary Search Tree

---
+++ depth-first-search-easy/733.flood-fill.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=733 lang=python3
+#
+# [733] Flood Fill
+#
+
+# @lc code=start
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        xlen = len(image)
+        ylen = len(image[0])
+        ori = image[sr][sc]
+
+        def dfs(x, y):
+            if 0 <= x < xlen and 0 <= y < ylen and image[x][y] == ori:
+                image[x][y] = color
+                dfs(x-1, y)
+                dfs(x+1, y)
+                dfs(x, y-1)
+                dfs(x, y+1)
+
+        if ori != color:
+            dfs(sr, sc)
+        return image
+# @lc code=end
+
