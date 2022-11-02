c51397ce1b0d83ea28b71ffdb645cb3c307d8c2a
scissor <scissorsmail@gmail.com>
2022-11-02 13:48:27 +0800

    Add new solution

--- dynamic-programming-easy/70.climbing-stairs.py
+++ dynamic-programming-easy/70.climbing-stairs.py
@@ -15,12 +15,10 @@ class Solution:
     
     # Self long solution
     def climbStairs(self, n: int) -> int:
-        if n == 1: return 1
-        
-        temp1 = 1
+        temp1 = 0
         temp2 = 1
         sum = 0
-        for i in range(n-1):
+        for i in range(n):
             sum = temp1 + temp2
             temp1 = temp2
             temp2 = sum
@@ -36,10 +34,10 @@ class Solution:
         return snd
     
     # Fib by list
-    def __init__(self):
-        self.fib = [0,1,2]
-        for i in range(3,46):
-            self.fib.append(self.fib[i-1] + self.fib[i-2]);
+    # def __init__(self):
+    #     self.fib = [0,1,2]
+    #     for i in range(3,46):
+    #         self.fib.append(self.fib[i-1] + self.fib[i-2]);
             
     def climbStairs_4(self, n: int) -> int:
         return self.fib[n]
@@ -48,6 +46,15 @@ class Solution:
     def climbStairs_5(self, n: int) -> int:
         d = {0: 0, 1: 1, 2: 2, 3: 3, 4: 5, 5: 8, 6: 13, 7: 21, 8: 34, 9: 55, 10: 89, 11: 144, 12: 233, 13: 377, 14: 610, 15: 987, 16: 1597, 17: 2584, 18: 4181, 19: 6765, 20: 10946, 21: 17711, 22: 28657, 23: 46368, 24: 75025, 25: 121393, 26: 196418, 27: 317811, 28: 514229, 29: 832040, 30: 1346269, 31: 2178309, 32: 3524578, 33: 5702887, 34: 9227465, 35: 14930352, 36: 24157817, 37: 39088169, 38: 63245986, 39: 102334155, 40: 165580141, 41: 267914296, 42: 433494437, 43: 701408733, 44: 1134903170, 45: 1836311903}
         return d[n]
+    
+    # Top down + memorization (dictionary)  
+    def __init__(self):
+        self.dic = {1:1, 2:2}
+        
+    def climbStairs(self, n):
+        if n not in self.dic:
+            self.dic[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.dic[n]
         
 # @lc code=end
 
