dcb094aa7806f67c5a3eecda9e72009f65ef8194
scissor <scissorsmail@gmail.com>
2022-11-09 11:10:17 +0800

    [100] Same Tree

--- bit-manipulation-easy/338.counting-bits.py
+++ bit-manipulation-easy/338.counting-bits.py
@@ -6,7 +6,7 @@
 
 # @lc code=start
 class Solution:
-    def countBits(self, n: int) -> List[int]:
+    def countBits_4(self, n: int) -> List[int]:
         if n == 0: return [0]
         
         ret = [0, 1]
@@ -35,7 +35,7 @@ class Solution:
         return bits[:n+1]
     
     # Self solution by list concat
-    def countBits_1(self, n: int) -> List[int]:
+    def countBits(self, n: int) -> List[int]:
         subset = [1]
         bits = [0, 1]
 
