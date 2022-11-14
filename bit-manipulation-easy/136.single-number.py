dc653ac1519c9fe65d8c79797e2eb60ef31ba261
scissor <scissorsmail@gmail.com>
2022-11-14 11:57:40 +0800

    [136] Single Number

--- bit-manipulation-easy/136.single-number.py
+++ bit-manipulation-easy/136.single-number.py
@@ -5,12 +5,6 @@
 #
 
 # @lc code=start
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     # Math magic
     def singleNumber(self, nums: List[int]) -> int:
